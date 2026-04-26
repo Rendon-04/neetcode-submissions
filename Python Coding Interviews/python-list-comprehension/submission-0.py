cc0bcbe72d32675282aba0e21c91bc5fbd15f56d
Ivan Rendon <134645875+Rendon-04@users.noreply.github.com>
2026-04-26 16:04:10 -0700

    Add: python-list-comprehension - submission-0

---
+++ Python Coding Interviews/python-list-comprehension/submission-0.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:
+    arr = []
+    for n in range(1, n + 1):
+        if n % 2 != 0:
+            arr.append(n)
+    return arr
+   
+
+
+# do not modify below this line
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))
